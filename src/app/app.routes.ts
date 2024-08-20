f1c9be0947bca943cd45abb31df39a7f4ff0a146
Nick.Hsu <Nick.Hsu@testritegroup.com>
2024-08-20 09:53:57 +0800

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { GeminiChatConsoleComponent } from './gemini-chat-console/gemini-chat-console.component';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'GeminiChatConsole',
+    component: GeminiChatConsoleComponent,
+    providers: [
+      importProvidersFrom(HttpClientModule)
+    ],
+  },
+];
