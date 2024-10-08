f0e9a0a36b52faede9092dc4675185c417852744
Nick.Hsu <Nick.Hsu@testritegroup.com>
2024-10-08 15:10:54 +0800

    fix: 修正不能在server side 調用webSocket因應hydration架構調整.

--- src/app/gemini-chat-console/gemini-chat-console.component.ts
+++ src/app/gemini-chat-console/gemini-chat-console.component.ts
@@ -1,7 +1,8 @@
-import { Component, importProvidersFrom } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AsyncPipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { AsyncPipe, isPlatformBrowser } from '@angular/common';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-gemini-chat-console',
@@ -14,24 +15,74 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './gemini-chat-console.component.html',
   styleUrl: './gemini-chat-console.component.sass'
 })
-export class GeminiChatConsoleComponent {
+export class GeminiChatConsoleComponent implements OnInit, OnDestroy {
+
+  private chatUrl = 'ws://localhost:8080/chat'
+
+  generatedText = '';
+  userInput = '';
+  socket: WebSocketSubject<any> | undefined;
+
+
+
+
   constructor(
-    private http: HttpClient
-  ) {}
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.socket = webSocket({
+        url: this.chatUrl,
+        binaryType: 'blob',
+        deserializer: (event) => {
+          if (typeof event.data === 'string') {
+            return event.data;
+          }
+          return new Error('Unsupported data type');
+        }
+      });
+      this.connectToChat();
+    }
+  }
 
-  userInput: string = '';
-  generatedText: string = '';
+  ngOnInit() {
 
-  sendQuestion() {
-    this.http.post('http://localhost:8080/chat', this.userInput, {
-      responseType: 'text',
-    }).subscribe(
-      (text) => {
-        this.generatedText += text; // Append the received text
-      },
-      (error) => {
-        console.error('Error:', error);
-      }
-    );
+  }
+
+  connectToChat(): void {
+    if (isPlatformBrowser(this.platformId) && this.socket) {
+      this.socket.pipe(
+        tap({
+          next: (data: any) => {
+            console.log('Received message from server:', data);
+            this.generatedText += data;
+            // Handle the received message as needed
+          },
+          error: (error: any) => {
+            console.error('WebSocket error:', error);
+          }
+        })
+      ).subscribe();
+    }
+  }
+
+  sendQuestion(): void {
+    if (isPlatformBrowser(this.platformId) && this.socket) {
+      this.socket.next(this.userInput);
+    }
+  }
+
+  closeConnection(): void {
+    if (isPlatformBrowser(this.platformId) && this.socket) {
+      this.socket.complete();
+    }
+  }
+
+  ngOnDestroy() {
+    this.closeConnection();
   }
 }
+
+interface WebSocketMessageEvent extends MessageEvent {
+  data: Blob | ArrayBuffer | string;
+}
+
